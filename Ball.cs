86023160e392ed940af3f7499f497be2d4f9bd24
lillybo <lillian.bojkov@gmail.com>
2023-08-02 18:05:52 +0300

    GDB-56 destroying ball on click

---
+++ Ball.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void OnMouseDown() {
+        Destroy(gameObject);    
+    }
+}
